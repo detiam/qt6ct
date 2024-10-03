eafd83ffdbb52ad04dc6c34784655eded4d095b6
Ilya Fedin <fedin-ilja2010@ya.ru>
2024-10-03 05:57:10 +0400

    Don't rely on label font for saving font settings
    Fixes #25

--- src/qt6ct/fontspage.cpp
+++ src/qt6ct/fontspage.cpp
@@ -62,8 +62,8 @@ FontsPage::~FontsPage()
 void FontsPage::writeSettings(QSettings *settings)
 {
     settings->beginGroup("Fonts");
-    settings->setValue("general", m_ui->generalFontLabel->font().toString());
-    settings->setValue("fixed", m_ui->fixedFontLabel->font().toString());
+    settings->setValue("general", m_ui->generalFontLabel->property("value"));
+    settings->setValue("fixed", m_ui->fixedFontLabel->property("value"));
     settings->endGroup();
 }
 
@@ -81,6 +81,7 @@ void FontsPage::onFontChangeRequested(QWidget *widget)
             || font.styleName() == QLatin1String("Roman")))
         font.setStyleName(QString());
 
+    widget->setProperty("value", font.toString());
     widget->setFont(font);
     qobject_cast<QLabel*>(widget)->setText(font.family () + " " + QString::number(font.pointSize ()));
 }
@@ -101,6 +102,7 @@ void FontsPage::loadFont(QSettings *settings, QLabel *label, const QString &key)
         : QFontDatabase::systemFont(QFontDatabase::GeneralFont)).value<QFont>();
     label->setText(font.family() + " " + QString::number(font.pointSize()));
     label->setFont(font);
+    label->setProperty("value", font.toString());
 }
 
 void FontsPage::on_createFontsConfButton_clicked()
